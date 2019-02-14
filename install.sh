ccecb24bd876aafc64cd9ac83156045bef77b7ea
Felipe Ortega <felipe.ortega@csmarta.cl>
2019-02-14 12:22:10 -0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for i in $(ls -A)
+do
+    if [ $i != ".git" ] && [ $i != ".gitignore" ] && [ $i != "install.sh" ] && [ $i != "README.md" ]
+    then
+	if [ -h "$HOME/$i" ]
+	then
+	    echo "Skip $i"
+	    continue
+	elif [ -f "$HOME/$i" ] || [ -d "$HOME/$i" ]
+	then
+	    mv $HOME/$i $HOME/$i.backup
+	elif [ -a "$HOME/$i" ]
+	then
+	    echo "Cannot continue"
+	    stat $i
+	    break
+	fi
+		
+	ln -s $(pwd)/$i $HOME/$(basename $i)
+    fi
+done
